09ef326a78b5e40d110626460bb089786c228cee
OXGN <cabarrubias.nevinharold@gmail.com>
2024-02-28 15:54:47 +0800

    Minor Fix

--- view.php
+++ view.php
@@ -8,91 +8,96 @@ include ("config.php");
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-          crossorigin="anonymous">
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+        crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="./css/stylenav.css">
 </head>
 <body>
 
-<h1 class="text-center">Update Student Data</h1>
+<h1 class="text-center">Student's Data</h1>
+
+    <nav class="navigation">
+        <a href="./index.php">Home</a>
+        <a>></a>
+        <a >Student's Data</a>
+    </nav>
+
 <div class="container mt-4">
     <div class="row justify-content-center">
         <div class="col-lg-9">
 
-        <?php
-            if(isset($_GET['id']))
-            {
-                $id = $_GET['id'];
-                $users = "SELECT * FROM `student` WHERE `id` = '$id'";
-                $user_result = mysqli_query($con, $users);
-
-                if(mysqli_num_rows($user_result) > 0 )
-                {
-                    foreach($user_result as $user)
+            <!--  Start Accepting ID that is passed from view, then GET using the ID that is passed  -->
+                <?php
+                    if(isset($_GET['id']))
                     {
-        ?>
-                <form action="update.php" method="POST">
+                        $id = $_GET['id'];
+                        $users = "SELECT * FROM `student` WHERE `id` = '$id'";
+                        $user_result = mysqli_query($con, $users);
 
-                <input type="hidden" name="id" value="<?=$user['id'];?>">
+                        // Start Initializing data found in database using ID then extract
+                            if(mysqli_num_rows($user_result) > 0 )
+                            {
+                                foreach($user_result as $user)
+                                {
+                ?>
+            <!--  End Accepting ID that is passed from view, then GET using the ID that is passed  -->
 
-                <div class="row">
-                    <div class="col-md-12 mb-3">
-                        <label for="studentId" class="form-label">Student I.D</label>
-                        <input type="text" class="form-control" id="studentId" value="<?=$user['studentID'];?>" name="studentID" readonly>
-                    </div>
+                                <!-- Start Showing Results in Database-->
+                                    <div class="row">
+                                        <div class="col-md-12 mb-3">
+                                            <label for="studentId" class="form-label">Student I.D</label>
+                                            <h1 class="form-control" id="firstName">
+                                                <?=$user['studentID'];?>
+                                            </h1>
+                                        </div>
+                                        <div class="col-md-4 mb-3">
+                                            <label for="firstName" class="form-label">First Name</label>
+                                            <h1 class="form-control" id="firstName">
+                                                <?=$user['firstName'];?>
+                                            </h1>
+                                        </div>
+                                        <div class="col-md-4 mb-3">
+                                            <label for="middleName" class="form-label">Middle Name</label>
+                                            <input type="text" class="form-control" id="middleName" value="<?=$user['middleName'];?>" name="middleName" readonly>
+                                        </div>
+                                        <div class="col-md-4 mb-3">
+                                            <label for="lastName" class="form-label">Last Name</label>
+                                            <input type="text" class="form-control" id="lastName" value="<?=$user['lastName'];?>" name="lastName" readonly>
+                                        </div>
+                                        <div class="col-md-4 mb-3">
+                                            <label for="dateOfBirth" class="form-label">Date of Birth</label>
+                                            <input type="date" class="form-control" id="dateOfBirth" value="<?=$user['dateOfBirth'];?>" name="dateOfBirth" readonly>
+                                        </div>
+                                        <div class="col-md-4 mb-3">
+                                            <label for="email" class="form-label">Email Address</label>
+                                            <input type="text" class="form-control" id="email" value="<?=$user['emailAddress'];?>" name="email" readonly>
+                                        </div>
+                                        <div class="col-md-4 mb-3">
+                                            <label for="phoneNumber" class="form-label">Phone Number</label>
+                                            <input type="text" class="form-control" id="phoneNumber" value="<?=$user['phoneNumber'];?>" name="phoneNumber" readonly>
+                                        </div>
+                                        <div class="col-md-12 mb-3 text-center">
+                                            <a class="btn btn-primary" href="index.php" style="float: right;" ">Back</a>
+                                        </div>
+                                    </div>
+                                <!-- End Showing Results in Database-->
 
-                    <div class="col-md-4 mb-3">
-                        <label for="firstName" class="form-label">First Name</label>
-                        <input type="text" class="form-control" id="firstName" value="<?=$user['firstName'];?>" name="firstName" readonly>
-                    </div>
-
-                    <div class="col-md-4 mb-3">
-                        <label for="middleName" class="form-label">Middle Name</label>
-                        <input type="text" class="form-control" id="middleName" value="<?=$user['middleName'];?>" name="middleName" readonly>
-                    </div>
-
-                    <div class="col-md-4 mb-3">
-                        <label for="lastName" class="form-label">Last Name</label>
-                        <input type="text" class="form-control" id="lastName" value="<?=$user['lastName'];?>" name="lastName" readonly>
-                    </div>
-
-                    <div class="col-md-4 mb-3">
-                        <label for="dateOfBirth" class="form-label">Date of Birth</label>
-                        <input type="date" class="form-control" id="dateOfBirth" value="<?=$user['dateOfBirth'];?>" name="dateOfBirth" readonly>
-                    </div>
-
-                    <div class="col-md-4 mb-3">
-                        <label for="email" class="form-label">Email Address</label>
-                        <input type="text" class="form-control" id="email" value="<?=$user['emailAddress'];?>" name="email" readonly>
-                    </div>
-
-                    <div class="col-md-4 mb-3">
-                        <label for="phoneNumber" class="form-label">Phone Number</label>
-                        <input type="text" class="form-control" id="phoneNumber" value="<?=$user['phoneNumber'];?>" name="phoneNumber" readonly>
-                    </div>
-
-                    <div class="col-md-12 mb-3 text-center">
-                        <button type="submit" class="btn btn-primary"  style="float: right;" name="submit">Submit</button>
-                    </div>
-                    <div class="col-md-12 mb-3 text-center">
-                        <a class="btn btn-primary" href="index.php" style="float: right;" ">Back</a>
-                    </div>
-                </div>
-            </form>
         </div>
     </div>
 
-    <?php
-                }
-            }
-            else
-            {
-                ?>
-                <h4>No Record Found!</h4>
                 <?php
-            }
-        }
-?>
+                        }
+                        }
+                            else
+                        {
+                ?>
+                            <h4>No Record Found!</h4>
+                <?php
+                        }
+                    }
+                    // End Initializing data found in database using ID then extract
+                ?>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
